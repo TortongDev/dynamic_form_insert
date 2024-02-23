c07e9de1eba6f1bbc94f0b3f371257c23f00b020
TortongDev <kittithat.charoenkhet101@gmail.com>
2024-02-23 15:26:14 +0700

    add / create file project all to repo

---
+++ setup_tortong_script.bat
@@ -0,0 +1,5 @@
+mkdir "AppClone/";
+copy C:\laragon\www\TortongFramework\App\*.* C:\laragon\www\TortongFramework\AppClone\.
+copy C:\laragon\www\TortongFramework\*.* C:\laragon\www\TortongFramework\AppClone\. /Y
+
+del C:\laragon\www\TortongFramework\AppClone\setup_tortong_script.*
